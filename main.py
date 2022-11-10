99607dd8c8a708644549bd3170fb0dbfed78dd6f
Isak410 <Isakstenep@gmail.com>
2022-11-10 11:07:00 +0100

    Enemy
    asd

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ BLACK = (0,0,0)
 RED = (255,0,0)
 BLUE = (0,0,255)
 LIGHT_GRAY = (211,211,211)
+DARKBLUE = (25, 25, 112)
+
+spawn = 0
 
 all_sprites = pg.sprite.Group()
+enemy_group = pg.sprite.Group()
 
 hero = Player()
 all_sprites.add(hero)
 
 enemy = Enemy()
-all_sprites.add(enemy)
+enemy2 = Enemy2()
+enemy_group.add(enemy, enemy2)
 
 
 FPS = 120
@@ -34,13 +39,31 @@ while playing:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             playing = False
-    screen.fill(CYAN)
+    screen.fill(DARKBLUE)
 
 
 
     all_sprites.update()
-
     all_sprites.draw(screen)
+
+    hits = pg.sprite.spritecollide(hero, enemy_group,dokill=True)
+    if hits:
+        for hit in hits:
+            spawn += 1
+            print (spawn)
+
+
+    #lag nye fiender
+    if len(enemy_group) < 50009:
+        enemy = Enemy()
+        all_sprites.add(enemy)
+        enemy_group.add(enemy)
+
+ 
+
+
+
+  
 
 
     
--- sprites.py
+++ sprites.py
@@ -4,10 +4,13 @@ vec = pg.math.Vector2
 
 
 player_img = pg.image.load("kar.PNG")
-player_img = pg.transform.scale(player_img, (250,250))
+player_img = pg.transform.scale(player_img, (150,150))
 
 enemy_img = pg.image.load("last ned.PNG")
-enemy_img = pg.transform.scale(enemy_img, (250,250))
+enemy_img = pg.transform.scale(enemy_img, (150,150))
+
+enemy2_img = pg.image.load("last ned.PNG")
+enemy2_img = pg.transform.scale(enemy_img, (150,150))
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
@@ -18,24 +21,9 @@ class Player(pg.sprite.Sprite):
         self.rect.center = self.pos
         self.speed = 3
 
-class Enemy(pg.sprite.Sprite):
-    def __init__(self):
-        pg.sprite.Sprite.__init__(self)
-        self.image = enemy_img
-        self.rect = self.image.get_rect()     #hitbox
-        self.pos = vec(900,randint(0,600))
-        self.rect.center = self.pos
-        self.speed = 3
-
-
     def update(self):
         self.rect.center = self.pos # holder rect på player hver frame
-        self.pos.x -= self.speed
 
-
-
-
-        
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
             self.pos.y -= self.speed
@@ -49,17 +37,47 @@ class Enemy(pg.sprite.Sprite):
             self.speed = 5
         else:
             self.speed = 3
-            
-                            
-            
-        if self.pos.x > 770:
-            self.pos.x = 770
-        if self.pos.x < 0:
-            self.pos.x = 45
-        if self.pos.y > 717:
-            self.pos.y = 717
-        if self.pos.y < 45:
-            self.pos.y = 45
+
+        
+class Enemy(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = enemy_img
+        self.rect = self.image.get_rect()     #hitbox
+        self.pos = vec(900,randint(0,600))
+        self.rect.center = self.pos
+        self.speed = 3
+
+    def update(self):
+        self.rect.center = self.pos # holder rect på player hver frame
+        self.pos.x -= 4
+
+        if self.pos.x < -125:
+            self.pos.x = 900
+            self.pos.y = randint(100,800)
+
+class Enemy2(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = enemy_img
+        self.rect = self.image.get_rect()     #hitbox
+        self.pos = vec(randint(0,800), -100)
+        self.rect.center = self.pos
+        self.speed = 3
+
+    def update(self):
+        self.rect.center = self.pos # holder rect på player hver frame
+        self.pos.y += 4
+
+        if self.pos.y < 900:
+            self.pos.y = -125
+            self.pos.x = randint(100,700)
+
+
+        
+        
+        
+        
 
 
 
